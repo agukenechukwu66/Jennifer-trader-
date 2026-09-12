42c658eae64b5d4ff88bd79674c8ba6272afcf7a
agukenechukwu66 <agukenechukwu66@gmail.com>
2026-09-12 09:23:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
  const accountBtn = document.querySelector('.btn');
  const userIcon = document.querySelector('.user-icon');
 
+ if (accountBtn) {
  accountBtn.addEventListener('click', (e) => {
  alert('Create Account button clicked');
  });
+ }
 
+ if (userIcon) {
  userIcon.addEventListener('click', () => {
  alert('User icon clicked');
  });
+ }
 
  const registrationForm = document.getElementById('registration-form');
  if (registrationForm) {
